7fc51671231aa74ca57e65dfeab24c8ec23fb650
LUIS HENRIQUE LEAL RIES <98657828049@senacrs.edu.br>
2025-09-16 22:15:58 -0300

    Atualizar e Deletar no Service, junto com detalhamento no main

--- main.js
+++ main.js
@@ -12,32 +12,46 @@ async function main() {
         categoria: "Bebida",
         preco: 10.8
     };
+    //Inserindo produto1 (id=1)
     try{
-        await produtoService.inserir(produto1);
+        produto1 = await produtoService.inserir(produto1);
+        console.log("Produto inserido", produto1)
     } catch (err) {
         console.log("Erro: ", err.msg);
     }
+    //Inserindo produto2 (id=2)
     try {
-        await produtoService.inserir(produto2);
+        produto2 = await produtoService.inserir(produto2);
+        console.log("Produto inserido", produto2)
     } catch (err) {
         console.log("Erro: ", err.msg);
     }
+    //Inserindo produto3 (id=3)
     try{ 
-        await produtoService.inserir({nome:"Feijao", categoria:"Alimento", preco:6.7});
+        const produto3 = await produtoService.inserir({nome:"Feijao", categoria:"Alimento", preco:6.7});
+        console.log("Produto inserido", produto3);
     } catch (err) {
         console.log("Erro: ", err.msg);
     }
+    //Inserindo produto4 - Vai dar erro, pois não tem preço
     try {
-        await produtoService.inserir({nome:"Coca-cola", categoria:"Bebida"});
-            } catch (err) {
-        console.log("Erro: ", err.msg);
-    }
-    try {
-        await produtoService.inserir({nome:"Detergente", categoria:"Limpeza", preco:2.5});
+        const produto4 = await produtoService.inserir({nome:"Coca-cola", categoria:"Bebida"});
+        console.log("Produto inserido", produto4);
     } catch (err) {
         console.log("Erro: ", err.msg);
     }
+    //Inserindo produto5 (id=4)
+    try {
+        const produto5 = await produtoService.inserir({nome:"Detergente", categoria:"Limpeza", preco:2.5});
+        console.log("Produto inserido", produto5);
+    } catch (err) {
+        console.log("Erro: ", err.msg);
+    }
+    
+    //Lista de produtos inicial
+    console.log("Lista inicial: ", await produtoService.listar());
 
+    //Buscando produto com id=1 (Arroz)
     try {
         let produtoId1 = await produtoService.buscarPorId(1);
         console.log("Produto Id=1: ", produtoId1);
@@ -46,6 +60,7 @@ async function main() {
         console.log("Erro: ", err.msg);
     }
 
+    //Buscando produto com id=100 - Erro: Produto não encontrado!
     try {
         let produtoId100 = await produtoService.buscarPorId(100);
         console.log("Produto Id=100: ", produtoId100);
@@ -54,7 +69,23 @@ async function main() {
         console.log("Erro: ", err.msg);
     }
 
-    console.log(await produtoService.listar());
+    //Atualizar o preço do produto com id=3 (feijao)
+    try{
+        console.log("Atualizando ID=3",
+            await produtoService.atualizar(3, {nome:"Feijao", categoria:"Alimento", preco:8.5}));
+    } catch(err) {
+        console.log("Erro", err.msg);
+    }
+
+    //Deletar o produto com id=4 (detergente)
+    try{
+        console.log("Deletando o ID=4", await produtoService.deletar(4));
+    } catch(err) {
+        console.log("Erro", err.msg);
+    }
+
+    //Lista de produtos final (sem detergente e preco do feijao atualizado)
+    console.log("Lista final: ", await produtoService.listar());
 }
 
 main();
--- service/produto_service.js
+++ service/produto_service.js
@@ -26,9 +26,35 @@ async function buscarPorId(id) {
         throw { id: 404, msg: "Produto não encontrado!"};
     }
 }
+async function atualizar(id, produto) {
+    if(produto && produto.nome && produto.categoria && produto.preco) {
+        const produtoAtualizado = await produtoRepository.atualizar(id, produto);
+        if(produtoAtualizado) {
+            return produtoAtualizado;
+        }        
+        else {
+            throw {id:404, msg: "Produto não encontrado"};
+        }
+    }
+    else {
+        throw {id:400, msg: "Produto sem dados corretos"};
+    }
+}
+
+async function deletar(id) {
+    let produto = await produtoRepository.deletar(id);
+    if(produto) {
+        return produto;
+    }
+    else {
+        throw { id: 404, msg: "Produto não encontrado!" }
+    }
+}
 
 module.exports = {
     listar,
     inserir,
-    buscarPorId
+    buscarPorId,
+    atualizar,
+    deletar
 }
